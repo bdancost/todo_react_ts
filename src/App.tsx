0fe60e94e43560e42b104c34d32a1a93aeea0bd0
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-06-08 10:01:45 -0300

    Adicionando estrutura do código

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import * as C from "./App.styles";
+import { Item } from "./types/Item";
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: "Estudar Ingles", done: false },
+    { id: 2, name: "Estudar React", done: false },
+  ]);
   return (
     <C.Container>
       <C.Area>
