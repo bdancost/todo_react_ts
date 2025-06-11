52ef4493e57879b0d8513507a7f61e734b9e57e4
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-06-10 22:54:18 -0300

    Adicionando funcionalidades no todo list

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,23 @@ const App = () => {
     { id: 1, name: "Estudar Ingles", done: false },
     { id: 2, name: "Estudar React", done: false },
   ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false,
+    });
+    setList(newList);
+  };
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
 
-        <AddArea />
+        <AddArea onEnter={handleAddTask} />
 
         {list.map((item, index) => (
           <ListItem key={index} item={item} />
--- src/components/AddArea/index.tsx
+++ src/components/AddArea/index.tsx
@@ -1,10 +1,30 @@
+import { useState, KeyboardEvent } from "react";
 import * as C from "./styles";
 
-export const AddArea = () => {
+type Props = {
+  onEnter: (taskName: string) => void;
+};
+
+export const AddArea = ({ onEnter }: Props) => {
+  const [inputText, setInputText] = useState("");
+
+  const handleKeyUp = (e: KeyboardEvent) => {
+    if (e.code === "Enter" && inputText !== "") {
+      onEnter(inputText);
+      setInputText(""); // Clear the input field after adding the task
+    }
+  };
+
   return (
     <C.Container>
       <div className="image">╋</div>
-      <input type="text" placeholder="Adicionar tarefa" />
+      <input
+        type="text"
+        placeholder="Adicionar tarefa"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        onKeyUp={handleKeyUp}
+      />
     </C.Container>
   );
 };
