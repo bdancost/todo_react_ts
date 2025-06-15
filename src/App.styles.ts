ed1ffa9c80864c3bbfe4c973866e5bd4dfbd42fe
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-06-15 19:02:50 -0300

    Alterando o dark mode no H1

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -40,7 +40,7 @@ export const Area = styled.div`
 export const Header = styled.h1`
   margin: 0;
   padding: 0;
-  color: #fff;
+  color: ${(props) => props.theme.header};
   text-align: center;
   border-bottom: 1px solid #444;
   padding-bottom: 20px;
